d82b5535eb8ccd8f2a37f8639265c9c80ee83918
Jonathan Ligh <JonathankLigh@gmail.com>
2016-07-06 17:35:39 -0400

    ?

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -1,5 +1,41 @@
 'use strict';
 
-juke.factory('PlayerFactory', function(){
+juke.factory('PlayerFactory', function($http, $log){
   // non-UI logic in here
+  	var obj = {};
+  	var audio = document.createElement('audio');
+  	obj.start = function(song){
+  		audio.src = song;
+  		audio.load();
+  		audio.play();
+  	}
+
+  	obj.pause = function(){
+
+  	}
+
+  	obj.resume = function(){
+
+  	}
+
+  	obj.isPlaying = function(){
+
+  	}
+
+  	obj.getCurrentSong = function(){
+
+  	}
+
+  	obj.next = function(){
+
+  	}
+
+  	obj.previous = function(){
+
+  	}
+
+  	obj.getProgress = function(){
+
+  	}
+	return obj;
 });
