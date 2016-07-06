07531b3b4ab2c2ff11085a9523d8e55119c79c5d
Jonathan Ligh <JonathankLigh@gmail.com>
2016-07-06 16:08:33 -0400

    .then still undefined

--- browser/js/album/album.controllers.js
+++ browser/js/album/album.controllers.js
@@ -1,23 +1,15 @@
 'use strict';
 
-juke.controller('AlbumCtrl', function ($scope, $http, $rootScope, $log) {
 
+juke.controller('AlbumCtrl', function ($scope, AlbumFactory, $rootScope, $log) {
+  
+
+  AlbumFactory.fetchAll()
+  .then(function(album){
+    $scope.album = album
+  })
   // load our initial data
-  $http.get('/api/albums/')
-  .then(function (res) { return res.data; })
-  .then(function (albums) {
-    return $http.get('/api/albums/' + albums[0].id); // temp: get one
-  })
-  .then(function (res) { return res.data; })
-  .then(function (album) {
-    album.imageUrl = '/api/albums/' + album.id + '/image';
-    album.songs.forEach(function (song, i) {
-      song.audioUrl = '/api/songs/' + song.id + '/audio';
-      song.albumIndex = i;
-    });
-    $scope.album = album;
-  })
-  .catch($log.error); // $log service can be turned on and off; also, pre-bound
+  // $log service can be turned on and off; also, pre-bound
 
   // main toggle
   $scope.toggle = function (song) {
---
+++ browser/js/album/album.factory.js
@@ -0,0 +1,28 @@
+
+
+juke.factory('AlbumFactory', function($http, $log){
+	var obj = {};
+
+  obj.fetchAll = function(){
+  	$http.get('/api/albums/')
+  	.then(function (res){ 
+  		res.data.forEach(function(e){
+  			e.imageUrl = '/api/albums/' + e.id + '/image';
+  			e.songs.forEach(function(song, i){
+  				song.audioUrl = '/api/songs/' + song.id + '/audio';
+  				song.albumIndex = i;
+  			})
+  		})
+  		return res.data; 
+  	})
+  }
+
+  obj.findById = function(id){
+  	$http.get('/api/albums/' + id)
+  	.then(function(album){
+  		return album;
+  	})
+  }
+
+  return obj;
+});
